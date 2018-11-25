024b9abde947f64444a420802bc99f7cfc3b7f85
slee172 <shlee8020@gmail.com>
2018-11-24 19:36:25 -0500

    Added scripts

---
+++ scripts/hostspecs.php
@@ -0,0 +1,13 @@
+<?php
+
+	# !!!!!
+	# !!!!! The one and ONLY place the active learning server is defined
+	# !!!!!
+	$port = 10000;
+	$host = "localhost";
+
+	#
+	#	IIP server
+	#
+	$IIPServer = "http://localhost/fcgi-bin/iipsrv.fcgi?";
+?>
