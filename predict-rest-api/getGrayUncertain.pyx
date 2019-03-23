6ab001a3ff14a6d79a693c3bf29518d880fd6303
slee172 <shlee8020@gmail.com>
2019-03-23 00:16:02 -0400

    Fixed uncertain issue

--- predict-rest-api/getGrayUncertain.pyx
+++ predict-rest-api/getGrayUncertain.pyx
@@ -19,51 +19,42 @@ def get(long fX, long fY, double[:, :] centX, double[:, :] centY, double[:] slid
     cdef long[:, :] grayClass = np.zeros((fX, fY), dtype=np.int)
     cdef double[:] scoreVec = np.ones((nrows,), dtype=np.double)
     cdef long i, row, col, curX, curY
-    cdef double scores, uncertainty
+    cdef double scores, uncertAbove, uncertBelow
     cdef double SREGION_GRID_SIZE = 80.0
-    cdef double KERN_SIZE = 7 * 11
     cdef double uncertMin, uncertMax
     cdef double classMin, classMax
 
     with nogil:
         for i in range(nrows):
+            curX = int(ceil(centX[i, 0] / SREGION_GRID_SIZE) - 1)
+            curY = int(ceil(centY[i, 0] / SREGION_GRID_SIZE) - 1)
             scores = (slideScores[i] * 2 ) - 1
             scoreVec[i] = 1 - fabs(scores)
 
-    sort_cpp(scoreVec)
-
-    cdef double uncertMedian = scoreVec[nrows / 2]
-
-    with nogil:
-        for i in range(nrows):
-            curX = int(ceil(centX[i, 0] / SREGION_GRID_SIZE) - 1)
-            curY = int(ceil(centY[i, 0] / SREGION_GRID_SIZE) - 1)
-            # uncertainty = 1 - fabs(slideScores[i, 0])
-            scores = (slideScores[i] * 2 ) - 1
-            uncertainty = 1 - fabs(scores)
-
-            if uncertainty > uncertMedian:
-                uncertainMap[curX, curY] += 1.0
+            uncertainMap[curX, curY] = max(uncertainMap[curX, curY], scoreVec[i])
 
             if scores >= 0:
                 classMap[curX, curY] += 1.0
 
             densityMap[curX, curY] += 1.0
 
+    sort_cpp(scoreVec)
+    cdef double uncertMedian = scoreVec[nrows / 2]
+
     with nogil:
         for row in range(fX):
             for col in range(fY):
                 if densityMap[row, col] == 0:
                     classMap[row, col] = 0
-                    uncertainMap[row, col] = 0
                 else:
                     classMap[row, col] = classMap[row, col] / densityMap[row, col]
-                    uncertainMap[row, col] = uncertainMap[row, col] / densityMap[row, col]
 
     uncertainMap = filters.gaussian(uncertainMap, 1)
     classMap = filters.gaussian(classMap, 1)
 
     uncertMax = uncertainMap[0, 0]
+    classMax = classMap[0, 0]
+
     with nogil:
         for i in range(fX):
             for j in range(fY):
@@ -71,15 +62,16 @@ def get(long fX, long fY, double[:, :] centX, double[:, :] centY, double[:] slid
                     uncertMax = uncertainMap[i, j]
                 if uncertainMap[i, j] < uncertMin:
                     uncertMin = uncertainMap[i, j]
-
-    classMax = classMap[0, 0]
-    with nogil:
-        for i in range(fX):
-            for j in range(fY):
                 if classMap[i, j] > classMax:
                     classMax = classMap[i, j]
                 if classMap[i, j] < classMin:
                     classMin = classMap[i, j]
+                if uncertainMap[i, j] > uncertMedian:
+                    uncertAbove += 1.0
+                else:
+                    uncertBelow += 1.0
+
+    cdef double uncertPercent = uncertAbove / (fY * fX)
 
     with nogil:
         for row in range(fX):
@@ -88,4 +80,4 @@ def get(long fX, long fY, double[:, :] centX, double[:, :] centY, double[:] slid
                 grayClass[row, col] = int(min(255.0 * classMap[row, col] / classMax, 255.0))
 
 
-    return np.asarray(grayUncertain), np.asarray(grayClass), uncertMin, uncertMax, uncertMedian, classMin, classMax
+    return np.asarray(grayUncertain), np.asarray(grayClass), uncertMin, uncertMax, uncertPercent, classMin, classMax
