90a99734be02181972ca753fcde3d1448d9cba41
slee172 <shlee8020@gmail.com>
2019-03-24 08:40:40 -0400

    Modified feature path

--- predict-rest-api/settings.py
+++ predict-rest-api/settings.py
@@ -19,5 +19,4 @@ class Settings():
         self.IS_HEATMAP = False
         self.TRAININGSET_DIR = "/localdata/classifiers/"
         self.DATASET_DIR = "/fastdata/features/"
-        self.PATH_TO_SPECIAL = "/fastdata/features/BRCA/BRCA-spfeatures-46-24-01.h5"
-      # self.PATH_TO_SPECIAL = "/fastdata/features/SKCM/SKCM-spfeatures-48-11-01.h5"
+        self.PATH_TO_SPECIAL = "/fastdata/features/BRCA/BRCA-spfeatures-1.h5"
